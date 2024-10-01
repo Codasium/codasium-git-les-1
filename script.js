1e0fda67fc25c46e74bab717914b342930f4f788
LK830052 <830052@student.lmc-vo.nl>
2024-10-01 12:40:47 +0200

    Added button

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let nameInput = prompt("Wat is je naam?");
 let nameElement = document.getElementById("name");
 nameElement.innerHTML = nameInput;
+let button = document.createElement("button");
+button.innerText = "woiow";
+button.addEventListener("click", function() {
+    alert("English or Spanish?");
+});
+
